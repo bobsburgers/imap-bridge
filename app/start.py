8b635619dbbfbdbe855f2190824217d252ca6f0a
risto <risto.kowaczewski@gmail.com>
2022-10-05 22:18:54 +0200

    Removed fastapi

---
+++ app/start.py
@@ -0,0 +1,47 @@
+import asyncio
+import logging
+
+from tracardi.domain.event_source import EventSource
+
+from app import config
+from app.bridge.email_checker import EMailChecker
+from app.service.local_ip import get_local_ip
+from app.service.main_loop import MainLoop
+
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+async def run_bridge(_shut_down):
+    checker = await EMailChecker.connect()
+
+    try:
+        while checker.run:
+            await checker.fetch_all_unseen()
+            await checker.wait_for_new_message()
+    except asyncio.CancelledError:
+        logger.info("Canceling task.")
+        await _shut_down()
+        # await checker.logout()
+
+
+bridge_type = 'rest' if config.bridge.event_transport == 'api' else 'imap'
+event_source = EventSource(
+    id=config.tracardi.source_id,
+    type=bridge_type,
+    name="IMAP Bridge",
+    description=f"IMAP Bridge at {get_local_ip()}",
+    tags=[bridge_type, 'bridge'],
+    transitional=False,
+    locked=True,
+    groups=["Bridge"],
+    manual="# Bridge description\n\nIMAP Bridge runs a imap worker in order to retrieve e-mails and push every new "
+           "e-mail to Tracardi.\n\n# Registration\n\nThis event source is automatically registered when bridge "
+           "starts."
+)
+
+try:
+    MainLoop(run_bridge, event_source)
+except KeyboardInterrupt:
+    print("Received exit, exiting")
